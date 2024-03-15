a6cedca8d88ca848214ef1c5856886ec8cd9ae5b
paigenpark <111656754+paigenpark@users.noreply.github.com>
2024-03-15 14:09:53 -0700

    adding cleaning files

--- code/sas2csv.R
+++ code/sas2csv.R
@@ -1,3 +1,10 @@
+library(rstudioapi)
+
+# Getting the path of your current open file
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+print( getwd() )
+
 options(stringsAsFactors=F, scipen = 999)
 
 pkg = 'Hmisc'
@@ -6,28 +13,28 @@ if (!require(pkg, character.only = TRUE)) {
   library(pkg, character.only = TRUE)
 }
 
-setwd()
+setwd("../data/brfss_data/")
 
-brfss <- sasxport.get("BRFSS_2015.XPT")
+brfss <- sasxport.get("LLCP2015.XPT ")
 write.csv(brfss, file = "brfss2015.csv")
 
-brfss <- sasxport.get("BRFSS_2016.XPT")
+brfss <- sasxport.get("LLCP2016.XPT ")
 write.csv(brfss, file = "brfss2016.csv")
 
-brfss <- sasxport.get("BRFSS_2017.XPT")
+brfss <- sasxport.get("LLCP2017.XPT ")
 write.csv(brfss, file = "brfss2017.csv")
 
-brfss <- sasxport.get("BRFSS_2018.XPT")
+brfss <- sasxport.get("LLCP2018.XPT ")
 write.csv(brfss, file = "brfss2018.csv")
 
-brfss <- sasxport.get("BRFSS_2019.XPT")
+brfss <- sasxport.get("LLCP2019.XPT ")
 write.csv(brfss, file = "brfss2019.csv")
 
-brfss <- sasxport.get("BRFSS_2020.XPT")
+brfss <- sasxport.get("LLCP2020.XPT ")
 write.csv(brfss, file = "brfss2020.csv")
 
-brfss <- sasxport.get("BRFSS_2021.XPT")
+brfss <- sasxport.get("LLCP2021.XPT ")
 write.csv(brfss, file = "brfss2021.csv")
 
-brfss <- sasxport.get("BRFSS_2022.XPT")
+brfss <- sasxport.get("LLCP2022.XPT ")
 write.csv(brfss, file = "brfss2022.csv")
